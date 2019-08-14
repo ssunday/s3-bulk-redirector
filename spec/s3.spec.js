1540b96d3d509b3d7186fd980254848583151ec7
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2019-08-13 19:22:34 -0500

    [LINT] Minor cleanup of other test files

--- spec/s3.spec.js
+++ spec/s3.spec.js
@@ -2,17 +2,21 @@ const s3 = require('../src/s3');
 
 describe('s3', () => {
   describe('applyRedirect', () => {
+    s3.client.putObject = jest.fn();
+
     it ('calls put object', () => {
-      s3.client.putObject = jest.fn();
       s3.applyRedirect('bucket', '/from', '/to');
 
-      expect(s3.client.putObject).toHaveBeenCalledWith({
-        ACL: 'public-read',
-        Body: '',
-        Bucket: 'bucket',
-        Key: '/from',
-        WebsiteRedirectLocation: '/to'
-      }, s3.handleError);
+      expect(s3.client.putObject).toHaveBeenCalledWith(
+        {
+          ACL: 'public-read',
+          Body: '',
+          Bucket: 'bucket',
+          Key: '/from',
+          WebsiteRedirectLocation: '/to'
+        },
+        s3.handleError
+      );
     });
   });
 });
